d5c88ea33ca328e4e71212d2ae128ca49391c7ff
Eduardo <eduardo_castillo97@hotmail.com>
2020-04-04 13:13:01 -0600

    Added user profile feed api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-
+from django.conf import settings
 
 class UserProfileManager(BaseUserManager):
     """ Manager for user profile"""
@@ -54,3 +54,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """ Returns a string representation of a user"""
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profiles status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as string"""
+        return self.status_text
--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -27,4 +27,9 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
         return user
 
-    
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """ Serailizers profile feed items """
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id','user_profile','status_text','created_on')
+        extra_kwargs = {'user_profile':{'read_only':True}}
